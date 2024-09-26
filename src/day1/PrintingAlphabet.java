eee2daca906ef5313ccac7d83c91fb69451d82f3
VandanaAlladi <alladi.vandana92@gmail.com>
2024-09-26 20:53:22 +0530

    Person Y adding

---
+++ src/day1/PrintingAlphabet.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class PrintingAlphabet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("a, b, c, d, e, f");
+	}
+
+}
