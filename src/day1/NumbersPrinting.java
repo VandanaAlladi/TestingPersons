27e41ca964ffe4f612112907a5e293d6a8eed74d
VandanaAlladi <alladi.vandana92@gmail.com>
2024-09-26 22:20:51 +0530

    into branch

--- src/day1/NumbersPrinting.java
+++ src/day1/NumbersPrinting.java
@@ -4,6 +4,7 @@ public class NumbersPrinting {
 
 	public static void main(String[] args) {
 		System.out.println("1,2,3,4,5,6,7,8,9");
+		System.out.println("11,12,13,14");
 		// TODO Auto-generated method stub
 
 	}
--- src/day1/PrintingAlphabet.java
+++ src/day1/PrintingAlphabet.java
@@ -5,6 +5,7 @@ public class PrintingAlphabet {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("a, b, c, d, e, f");
+		System.out.println("fjhdjfhjsdhfjsdhf");
 	}
 
 }
