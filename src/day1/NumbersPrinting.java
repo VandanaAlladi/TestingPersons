0e7bdfacff9d8d4efa25ae230c5b67b9091627a3
VandanaAlladi <alladi.vandana92@gmail.com>
2024-09-26 20:46:05 +0530

    person x commiting

---
+++ src/day1/NumbersPrinting.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class NumbersPrinting {
+
+	public static void main(String[] args) {
+		System.out.println("1,2,3,4,5,6,7,8,9");
+		// TODO Auto-generated method stub
+
+	}
+
+}
